aedf8041f2feba90d367d4f517cba0d30b8302ea
JereNurminen <jere.m.nurminen@outlook.com>
2016-11-25 17:21:27 +0200

    First "features"

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SIIKLI SUPER MEGA SHOPPING LIST' });
 });
 
 module.exports = router;
+
+function addItem (event) {
+  if (event.which == 13 || event.keyCode == 13) {
+    //code to execute here
+    return false;
+  }
+  return true;
+}
